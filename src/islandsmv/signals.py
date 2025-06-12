4958700a9c90eee9e9ceb73eb59f4ee7655f5920
Fauzaan Gasim <hello@fauzaanu.com>
2025-06-12 08:52:34 +0500

    bug: UTF-8 issues fix

--- src/islandsmv/signals.py
+++ src/islandsmv/signals.py
@@ -6,4 +6,4 @@ from islandsmv.apps import IslandsmvConfig
 def load_islands_fixture(sender, **kwargs):
     # only run for your app
     if sender.name == IslandsmvConfig.name:
-        call_command('loaddata', 'islands.json', app_label='islandsmv', verbosity=0)
+        call_command('loaddata', 'island.json', app_label='islandsmv', verbosity=0)
