7504cf40a4a0de05577c2b8a885633f326588a2b
Fauzaan Gasim <hello@fauzaanu.com>
2025-06-12 08:42:39 +0500

    Initial commit

---
+++ src/islandsmv/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+# Create your models here.
+class Atoll(models.Model):
+    code = models.CharField(max_length=10, primary_key=True)
+
+
+class Island(models.Model):
+    ADMIN_ISLAND = ("admin_island", "ADMIN ISLAND")
+    MAALE = ("maale", "MAALE")
+
+    ISLAND_TYPES = [
+        ADMIN_ISLAND, MAALE
+    ]
+
+    name = models.CharField(max_length=250)
+    island_name = models.CharField(max_length=250)
+    atoll = models.ForeignKey(Atoll, on_delete=models.CASCADE)
+    type = models.CharField(max_length=250, choices=ISLAND_TYPES)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+    @property
+    def lat_long_combined(self):
+        return self.latitude, self.longitude
