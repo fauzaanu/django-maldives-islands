52913ccb2fe1e8bdfe6cc41f2e9dc7098dcb3225
Fauzaan Gasim <hello@fauzaanu.com>
2025-06-14 09:31:12 +0500

    feat: Avoid repeated runs of load data command on each migration by checking for island count

--- src/islandsmv/signals.py
+++ src/islandsmv/signals.py
@@ -1,9 +1,11 @@
 from django.core.management import call_command
 
-from islandsmv.apps import IslandsmvConfig
+from .apps import IslandsmvConfig
+from .models import Island
 
 
 def load_islands_fixture(sender, **kwargs):
-    # only run for your app
     if sender.name == IslandsmvConfig.name:
-        call_command('loaddata', 'island.json', app_label='islandsmv', verbosity=0)
+
+        if Island.objects.all().count() == 0:
+            call_command('loaddata', 'island.json', app_label='islandsmv', verbosity=0)
